13d113d65aee2dfeac547e8eea6806d931ca70d1
justinealvaro15 <jpalvaro@up.edu.ph>
2019-11-01 18:55:59 +0800

    Added Home Screen

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import HomeScreen from './src/screens/HomeScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createStackNavigator({
+    Home: HomeScreen
+}, {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+        title: 'Health Food App'
+    }
 });
+
+export default createAppContainer(navigator);
+
