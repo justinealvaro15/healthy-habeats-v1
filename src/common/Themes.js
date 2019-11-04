8b03ebce040d674b09fe7c1aefacb9cf9c4bfa26
justinealvaro15 <jpalvaro@up.edu.ph>
2019-11-04 19:54:41 +0800

    Added font constant

--- src/common/Themes.js
+++ src/common/Themes.js
@@ -3,5 +3,9 @@ export const BACKGROUND_WHITE = '#FFFFFF';
 export const BACKGROUND_LIGHT_GRAY = '#EFF0F1';
 
 // containers
-export const CONTAINER_MARGIN = 15;
+export const CONTAINER_MARGIN = 12;
 export const CONTAINER_RADIUS = 10;
+
+// font
+export const FONT_SIZE_HEADER = 20; ///
+export const FONT_SIZE_REGULAR = 16.5;
