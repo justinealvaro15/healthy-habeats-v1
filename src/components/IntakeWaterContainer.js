0aefe8f938c5a8af2eb1b3751bea30e2038883ac
justinealvaro15 <jpalvaro@up.edu.ph>
2020-01-25 14:02:47 +0800

    Added max width for water intake container

--- src/components/IntakeWaterContainer.js
+++ src/components/IntakeWaterContainer.js
@@ -17,7 +17,7 @@ const IntakeWaterContainer = () => {
                         flexDirection: 'row'}}
                     >
                     <TouchableHighlight
-                        style={styles.button_add}
+                        style={styles.button}
                         onPress={() => water > 0 ? setWater(water-1) : null}
                     >
                         <View>
@@ -30,7 +30,7 @@ const IntakeWaterContainer = () => {
                     </View>
 
                     <TouchableHighlight
-                        style={styles.button_add}
+                        style={styles.button}
                         onPress={() => setWater(water+1)}
                     >
                         <View>
@@ -44,11 +44,11 @@ const IntakeWaterContainer = () => {
 };
 
 const styles = StyleSheet.create({
-    button_add: {
+    button: {
         backgroundColor: ThemeConstants.MAIN_YELLOW,
         borderRadius: ThemeConstants.CONTAINER_RADIUS,
         marginVertical: ThemeConstants.CONTAINER_MARGIN/2,
-        paddingHorizontal: ThemeConstants.CONTAINER_MARGIN
+        width: 50
     },
     container: {
         backgroundColor: ThemeConstants.MAIN_BLUE,
@@ -63,7 +63,8 @@ const styles = StyleSheet.create({
         backgroundColor: ThemeConstants.MAIN_WHITE,
         borderRadius: ThemeConstants.CONTAINER_RADIUS,
         marginVertical: ThemeConstants.CONTAINER_MARGIN/2,
-        paddingHorizontal: ThemeConstants.CONTAINER_MARGIN*1.5
+        paddingHorizontal: ThemeConstants.CONTAINER_MARGIN*1.5,
+        maxWidth: 160
     },
     text_button: {
         alignContent: 'center',
@@ -81,6 +82,7 @@ const styles = StyleSheet.create({
         paddingTop: ThemeConstants.CONTAINER_MARGIN*1.25
     },
     text_water: {
+        textAlign: 'center',
         fontSize: ThemeConstants.FONT_SIZE_REGULAR,
         marginVertical: ThemeConstants.CONTAINER_MARGIN/2
     }
