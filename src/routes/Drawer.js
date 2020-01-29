0dffb6484c76ef398c2c90d435b32f7e542e3cef
justinealvaro15 <jpalvaro@up.edu.ph>
2020-01-29 22:44:34 +0800

    Added style to drawer

--- src/routes/Drawer.js
+++ src/routes/Drawer.js
@@ -1,19 +1,59 @@
-import { createAppContainer } from 'react-navigation';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import React from 'react';
+import { createAppContainer, createDrawerNavigator, DrawerItems } from 'react-navigation';
+import { Image, ScrollView, StyleSheet, View, Text } from 'react-native';
 
 import HomeStack from './HomeStack';
 import UserProfileStack from './UserProfileStack';
 import TestScreen from '../screens/TestScreen';
 
+import { Feather } from '@expo/vector-icons';
+import { MAIN_BLUE, MAIN_WHITE } from '../common/Themes';
+import Constants from 'expo-constants';
+
 const RootDrawerNavigator = createDrawerNavigator({
     Home: {
         screen: HomeStack,
+        navigationOptions: {
+            drawerLabel: 'Home',
+            drawerIcon: ({tintColor}) => <Feather name='home' size={20} color={tintColor}/>
+        }
     },
     UserProfile: {
         screen: UserProfileStack,
+        navigationOptions: {
+            drawerLabel: 'Profile',
+            drawerIcon: ({tintColor}) => <Feather name='user' size={20} color={tintColor}/>
+        }
     },
     Test: {
-        screen: TestScreen
+        screen: TestScreen,
+        navigationOptions: {
+            drawerLabel: 'Test',
+            drawerIcon: ({tintColor}) => <Feather name='alert-triangle' size={20} color={tintColor}/>
+        }
+    }
+}, {
+    contentComponent: (props) => (
+        <View style={{ flex: 1 }}>
+            <Image source={require('../../assets/icon.png')} style={styles.logo}/>
+
+            <ScrollView>
+                <DrawerItems {...props}/>
+            </ScrollView>
+        </View> 
+    ),
+    contentOptions: {
+        activeBackgroundColor: MAIN_BLUE,
+        activeTintColor: MAIN_WHITE,
+    }
+});
+
+const styles = StyleSheet.create({
+	logo: {
+        alignSelf: 'center',
+        marginTop: Constants.statusBarHeight*1.5,
+		height: 150,
+        width: 150,
     }
 });
 
